34bf28fdb262aa0aa5c3cf53f2f05117b3232972
Eivind Støland <eivinsto@student.matnat.uio.no>
2020-08-25 10:49:31 +0200

    Added .gitignore

--- test_calculator.py
+++ test_calculator.py
@@ -8,3 +8,7 @@ def test_add():
 def test_add_floats():
     eps = 1e-14     #Error limit to account for machine precision
     assert calculator.add(0.1,0.2)-0.3<eps
+
+"Testing add function from calculator.py with strings"
+def test_add_strings():
+    assert calculator.add("Hello ","World")=="Hello World"
