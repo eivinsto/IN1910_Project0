ac9238cfb8b41487b42712530265e7fd12f9e6d5
Eivind Støland <eivinsto@student.matnat.uio.no>
2020-08-25 10:45:49 +0200

    Added test_add_floats() in test_calculator.py (Exercise 2)

--- test_calculator.py
+++ test_calculator.py
@@ -3,3 +3,8 @@ import calculator
 "Testing add function from calculator.py"
 def test_add():
     assert calculator.add(1,2)==3
+
+"Testing add function from calculator.py with floats"
+def test_add_floats():
+    eps = 1e-14     #Error limit to account for machine precision
+    assert calculator.add(0.1,0.2)-0.3<eps
